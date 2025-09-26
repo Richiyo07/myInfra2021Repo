cb602019bcbcdf4758e70321275d665654133656
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-09-26 17:26:19 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-s3-bucket-for-tf-state"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-yoh-Dyna-table"
+    dynamodb_table = "my-dynamo-db-table-tf"
   }
 }
