4e1a847328467c506eb56e5176682be4e0e3c8c0
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-10-01 00:39:45 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "richikey" 
+    default     =  "kennedykey" 
 }
 
 variable "instance_type" { 
