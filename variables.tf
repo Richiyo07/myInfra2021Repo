0e7af4f116bf8bcafdb84f6d1afa9f53a4f0a136
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-10-02 22:55:34 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-2" 
+       default     = "us-east-1" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "kennedykey" 
+    default     =  "myjenkinkey3" 
 }
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.micro" 
+    default     =  "t2.small" 
 }
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+    default     = "my-jenkins-security-group-2023" 
 }
 
 variable "tag_name" { 
@@ -24,28 +24,5 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0cfde0ea8edd312d4" 
-}
-variable "versioning" {
-    type        = bool
-    description = "(Optional) A state of versioning."
-    default     = true
-}
-variable "acl" {
-    type        = string
-    description = " Defaults to private "
-    default     = "private"
-}
-variable "bucket_prefix" {
-    type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-yohyoh-s3-bucket-2"
-}
-variable "tags" {
-    type        = map
-    description = "(Optional) A mapping of tags to assign to the bucket."
-    default     = {
-        environment = "DEV"
-        terraform   = "true"
-    }
+    default     ="ami-053b0d53c279acc90"
 }
