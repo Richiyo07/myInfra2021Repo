e37b0e3376b745ae6af9053a2060991f5cf50ea7
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-10-02 22:52:47 -0400

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,17 +1,13 @@
-resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0cfde0ea8edd312d4"
-      key_name = var.key_name
-      instance_type = "t2.micro"
-      vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
-      tags= {
-        Name = "sonar_instance"
-      }
-    }
-
- resource "aws_security_group" "sonar-sg-2022" {
-      name        = "security_sonar_group_2022"
+resource "aws_vpc" "sonar" {
+  cidr_block = "172.16.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "sonar_vpc"
+  }
+}
+ resource "aws_security_group" "security_sonar_group_2023" {
+      name        = "security_sonar_group_2023"
       description = "security group for Sonar"
-
       ingress {
         from_port   = 9000
         to_port     = 9000
@@ -25,25 +21,33 @@ resource "aws_instance" "mySonarInstance" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
       }
-
-     # outbound from Sonar server
+  # outbound from Sonar server
       egress {
         from_port   = 0
         to_port     = 65535
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
       }
-
-      tags= {
+        tags= {
         Name = "security_sonar"
       }
     }
+resource "aws_instance" "mySonarInstance" {
+      ami           = "ami-026ebd4cfe2c043b2"
+      key_name = "myjenkinkey3"
+      instance_type = "t2.small"
+      vpc_security_group_ids = [aws_security_group.security_sonar_group_2023.id]
 
-# Create Elastic IP address for Sonar instance
-resource "aws_eip" "mySonarInstance" {
-  vpc      = true
-  instance = aws_instance.mySonarInstance.id
-tags= {
+      tags= {
+        Name = "sonar_instance"
+      }
+    }
+
+  # Create Elastic IP address for Sonar instance
+   resource "aws_eip" "mySonarInstance" {
+   vpc = true
+   instance = aws_instance.mySonarInstance.id
+   tags= {
     Name = "sonar_elastic_ip"
   }
 }
