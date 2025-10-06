ad5a3622d8a3966e359b4cd406487dcb01a74aec
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-10-06 00:12:27 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,25 +1,9 @@
-<<<<<<< HEAD
-# Configure the AWS provider
-provider "aws" {
-  region = "us-east-1" # Specify your desired AWS region
-}
-
-# Define an S3 bucket resource
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345" # Replace with a globally unique name
-  acl    = "private"                       # Set access control list (e.g., private, public-read)
-
-  tags = {
-    Name        = "MyApplicationBucket"
-    Environment = "Development"
-=======
-resource "aws_s3_bucket" "my-s3-bucket" {
+resource "aws_s3_bucket" "my-yohyoh-s3-bucket-2" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
    versioning {
     enabled = var.versioning
->>>>>>> d1d8d507f5216990d94052769d477773a87740ca
   }
   
   tags = var.tags
