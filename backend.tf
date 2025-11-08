a3cc3a556d7750409f78105d1da406be56e0a91e
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-11-08 00:24:27 -0500

    Change S3 backend key from 'main' to 'master'

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-bucket-tf-state-june-2005"
-    key = "main"
+    key = "master"
     region = "us-east-2"
     dynamodb_table = "my-dynamo-db-table"
   }
