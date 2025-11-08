79687a2197dcf70ead7b92d6c731b16f4ceee1b1
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-11-08 01:49:09 -0500

    Update S3 bucket and DynamoDB table names

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-bucket-tf-state-june-2005"
+    bucket = "my-aws-s3-bucket-rich"
     key = "master"
     region = "us-east-2"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "my-dynamo-db-table-tf-rd"
   }
 }
