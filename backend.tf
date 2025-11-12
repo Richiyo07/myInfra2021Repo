76e34f2caf7a27b8f446a9fd14b73f1d1e035879
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-11-11 22:50:22 -0500

    Change S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-s3-bucket-yohyoh"
+    bucket = "my-aws-s3-bucket-rich"
     key = "master"
     region = "us-east-2"
     dynamodb_table = "my-dynamo-db-table-tf-kd"
