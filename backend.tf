e823ac993d3bb79357eef1fe9a2d802d4bb3d98c
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-11-11 23:32:28 -0500

    Change Terraform backend region from us-east-1 to us-east-2

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-aws-s3-bucket-rich"
     key = "master"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "my-dynamo-db-table-tf-kd"
   }
 }
